307150098d642d1db45c35622c12be0d13255fd7
Devinson Alvarez <dalvarez@sistemasmitdev.com>
2023-08-16 08:56:36 -0500

    Typed Double Linked Node

--- src/Lists/DoubleNode.php
+++ src/Lists/DoubleNode.php
@@ -4,74 +4,120 @@ declare(strict_types=1);
 
 namespace Devinson\Architect\Lists;
 
+/**
+ * @template TData
+ */
 class DoubleNode
 {
     /**
-     * Value stored in the node
-     */
-    protected mixed $data;
-
-    /**
      * Next node pointed to
+     * 
+     * @var null|DoubleNode<TData>
      */
-    protected ?DoubleNode $next;
+    protected ?DoubleNode $next = null;
 
     /**
      * Previous node pointed to
+     * 
+     * @var null|DoubleNode<TData>
      */
-    protected ?DoubleNode $prev;
+    protected ?DoubleNode $prev = null;
 
-    public function __construct(mixed $data)
+    /**
+     * @param TData $data
+     */
+    public function __construct(protected $data)
     {
-        $this->data = $data;
-        $this->next = null;
-        $this->prev = null;
     }
 
     /**
      * Sets the value contained in the node
+     * 
+     * @param TData $data
      */
-    public function setData(mixed $data): void
+    public function setData($data): void
     {
         $this->data = $data;
     }
 
     /**
      * Returns the values stored in the node
+     * 
+     * @return TData
      */
-    public function getData(): mixed
+    public function getData()
     {
         return $this->data;
     }
 
     /**
      * Sets the next node to point to
+     * 
+     * @param null|TData|DoubleNode<TData> $next
      */
-    public function setNext(mixed $next): void
+    public function setNext($next): void
     {
-        $this->next = $next;
+        if ($next instanceof DoubleNode) {
+            $this->next = $next;
+        } else if ($next === null) {
+            $this->next = null;
+        } else {
+            $this->next = new DoubleNode($next);
+        }
+    }
+
+    /**
+     * Return the data that contains the next node
+     * 
+     * @return null|TData
+     */
+    public function getNext()
+    {
+        return $this->next?->getData();
     }
 
     /**
      * Return the next node pointed to
+     * 
+     * @return null|DoubleNode<TData>
      */
-    public function getNext(): ?DoubleNode
+    public function getNextNode(): ?DoubleNode
     {
         return $this->next;
     }
 
     /**
      * Sets the previous node to point to
+     * 
+     * @param null|TData|DoubleNode<TData> $prev
      */
-    public function setPrev(mixed $prev): void
+    public function setPrev($prev): void
     {
-        $this->prev = $prev;
+        if ($prev instanceof DoubleNode) {
+            $this->prev = $prev;
+        } else if ($prev === null) {
+            $this->prev = null;
+        } else {
+            $this->prev = new DoubleNode($prev);
+        }
+    }
+
+    /**
+     * Return the data that contains the previous node
+     * 
+     * @return null|TData
+     */
+    public function getPrev()
+    {
+        return $this->prev?->getData();
     }
 
     /**
      * Return the previous node pointed to
+     * 
+     * @return null|DoubleNode<TData>
      */
-    public function getPrev(): ?DoubleNode
+    public function getPrevNode(): ?DoubleNode
     {
         return $this->prev;
     }
